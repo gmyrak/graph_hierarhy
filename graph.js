a7b66a083ec9b9f5c92e33ee017c8b50546fa229
gmyrak@gmail.com <gmyrak@gmail.com>
2020-02-28 23:55:35 +0200

    1

--- graph.js
+++ graph.js
@@ -7,8 +7,6 @@ function get_matrix(mm_text) {
 class BinMatrix {
 
     constructor(code) {
-
-
         this.size  = code.match(/\d+/g).reduce((M, curr)=> +curr>+M ? curr: M , -1);
         this.data = [];
         for (let i=0; i<this.size; i++) {
@@ -103,10 +101,108 @@ class BinMatrix {
             res = res.plus(exp);
             exp = exp.mult(this);
         }
+        for (let i=0; i<this.size; i++) res.data[i][i] = true;
         return res;
     }
 
+    hr() {
+        const md = this.reach();
+        const N = md.size;
+        const tab = {};
+        const levels = [];
+        
+        for (let i=1; i<=N; i++) {
+            let next = new Set([]);
+            let prev = new Set([]);
+            for (let j=0; j<N; j++) {
+                if (md.data[i-1][j]) next.add(j+1);
+            }
 
+            for (let j=0; j<N; j++) {
+                if (md.data[j][i-1]) prev.add(j+1);
+            }
 
+            tab[i] = {next, prev, common: next.inter(prev) };
+        }
+
+        while (true) {
+            let curr = new Set([]);
+            for (const i in tab) {
+                if (tab[i]['prev'].eq(tab[i]['common'])) curr = curr.union(tab[i]['prev']);
+            }
+
+            for (const i in tab) {
+                tab[i].prev.minus(curr);
+                tab[i].next.minus(curr);
+                tab[i].common.minus(curr);
+            }
+            levels.push(curr.arr());
+            for(const d of curr.arr()) delete tab[d];
+            if (Object.keys(tab).length === 0) break;
+        }
+
+        return levels;
+    }
+}
+
+function code_levels(levels) {
+    let code = '';
+    for(let i=0; i<levels.length; i++) {
+        const curr = levels[i];
+        code += `\nsubgraph L${i+1}\n${curr.join(' & ')}\nend\n`;
+    }
+    return code;
+}
+
+class Set {
+    constructor (arr) {
+        this.data = {};
+        if (typeof arr === 'object') {
+            for(const item of arr) {
+                this.data[item] = true;
+            }
+        } else {
+            this.data[arr] = true;
+        }
+    }
+
+    add(item) {
+        this.data[item] = true;
+    }
+
+    arr() {
+        return Object.keys(this.data);
+    }
+
+    include(item) {
+        return this.data[item];
+    }
+
+    subs(set) {
+        for(const item in set.data) {
+            if (! this.data[item]) return false;
+        }
+        return true;
+    }
+
+    eq(set) {
+        return this.subs(set) && set.subs(this);
+    }
+
+    minus(set) {
+        for (const item in set.data) delete this.data[item];
+    }
+
+    inter(set) {
+        const res = new Set([]);
+        for (const item in this.data) {
+            if (set.data[item]) res.add(item);
+        }
+        return res;
+    }
+
+    union(set) {
+        return new Set([...this.arr(), ...set.arr()]);
+    }
 
 }
